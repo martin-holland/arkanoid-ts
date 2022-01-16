c5861d8dd0a391302bc1882429c1b0becfc3f719
s2100688 <89017210+s2100688@users.noreply.github.com>
2022-01-16 23:02:04 +0200

    second commit

--- src/view/CanvasView.ts
+++ src/view/CanvasView.ts
@@ -3,3 +3,28 @@
 import { Brick } from '../sprites/Brick'
 import { Ball } from '../sprites/Ball'
 import { Paddle } from '../sprites/Paddle'
+
+export class CanvasView {
+    private canvas: HTMLCanvasElement;
+    private context: CanvasRenderingContext2D | null;
+    private scoreDisplay: HTMLObjectElement | null;
+    private start: HTMLObjectElement | null;
+    private info: HTMLObjectElement | null;
+
+    constructor(canvasName: string) {
+        this.canvas = document.querySelector(canvasName) as HTMLCanvasElement;
+        this.context = this.canvas.getContext('2d');
+        this.scoreDisplay = document.querySelector('#score');
+        this.start = document.querySelector('#start')
+        this.info = document.querySelector('#info')
+    }
+
+    clear(): void {
+        this.context?.clearRect(0,0,this.canvas.width, this.canvas.height) //clears the canvas starting at the top the optional chaining returns undefined if it cannot find it.
+    }
+
+    initStartButton(startFunction: (view: CanvasView) => void): void {
+        
+    }
+
+}
